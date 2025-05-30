d233a6110634410fce1a026882a142c3a401997f
Danny Li <lidn405@gmail.com>
2025-05-29 20:30:39 -0400

    changed locations of images

--- react-ts-app/src/pages/Home.tsx
+++ react-ts-app/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { ImageSlider } from "../components/Slider/ImageSlider";
-import temp1 from "../../public/sliderImages/temp1.svg"
-import temp2 from "../../public/sliderImages/temp2.svg"
-import temp3 from "../../public/sliderImages/temp3.svg"
+import temp1 from ".././components/Slider/sliderImages/temp1.svg"
+import temp2 from ".././components/Slider/sliderImages/temp2.svg"
+import temp3 from ".././components/Slider/sliderImages/temp3.svg"
 
 const images = [temp1, temp2, temp3]
 
