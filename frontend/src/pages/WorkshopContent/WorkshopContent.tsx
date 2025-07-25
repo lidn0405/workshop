5bdced402536c0ae717813d711b20548193e862f
Danny Li <lidn405@gmail.com>
2025-07-24 20:17:38 -0400

    minor workshop content changes

--- frontend/src/pages/WorkshopContent/WorkshopContent.tsx
+++ frontend/src/pages/WorkshopContent/WorkshopContent.tsx
@@ -1,7 +1,28 @@
-function WorkshopContent() {
-    return (
-        <div>
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import type { Workshop } from "../../types/workshop.types";
+import { getWorkshop } from "../../api/workshopApi";
 
+import "./workshop_content.css";
+
+function WorkshopContent() {
+    const {id} = useParams();
+    const workshop_id: number = Number(id);
+
+    const [workshop, setWorkshop] = useState<Workshop>();
+
+    useEffect(() => {
+        const getWorkshopData = async () => {
+            const res = await getWorkshop(workshop_id);
+            setWorkshop(res);
+        }
+
+        getWorkshopData();
+    }, [])
+
+    return (
+        <div className="content_section">
+            <p>{workshop?.subject}</p>    
         </div>
     )
 }
